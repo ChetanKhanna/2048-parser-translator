0c9d4b0937290661f3ec83b5729029263fd59399
chetan22 <ck.chetan20@gmail.com>
2021-04-15 14:09:50 +0530

    Added moving names feature

--- game.py
+++ game.py
@@ -2,28 +2,35 @@ import random
 import copy
 
 class Board:
-    def __init__(self, board=None):
-        if not board:
-            self.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-        else:
-            self.board = board
+    def __init__(self):
+        self.board = [
+            [[0, set()], [0, set()], [0, set()], [0, set()]],
+            [[0, set()], [0, set()], [0, set()], [0, set()]],
+            [[0, set()], [0, set()], [0, set()], [0, set()]],
+            [[0, set()], [0, set()], [0, set()], [0, set()]],
+        ]
+        self.all_names = set()
         self.changed = False
         self.empty_cells = []
         for i in range(4):
             for j in range(4):
                 self.empty_cells.append((i,j))
-        self.name_map = dict()
     
     def fill_random_tile(self):
         row, col = random.choice(self.empty_cells)
         if random.uniform(0,1) < 0.6:
-            self.board[row][col] = 2
+            self.board[row][col] = [2, self.board[row][col][1]]
         else:
-            self.board[row][col] = 4
+            self.board[row][col] = [4, self.board[row][col][1]]
         self.empty_cells.remove((row, col))
     
     def initiate_new_board(self):
-        self.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        self.board = [
+            [[0, set()], [0, set()], [0, set()], [0, set()]],
+            [[0, set()], [0, set()], [0, set()], [0, set()]],
+            [[0, set()], [0, set()], [0, set()], [0, set()]],
+            [[0, set()], [0, set()], [0, set()], [0, set()]],
+        ]
         self.empty_cells = []
         for i in range(4):
             for j in range(4):
@@ -34,7 +41,7 @@ class Board:
         self.empty_cells = []
         for i in range(4):
             for j in range(4):
-                if self.board[i][j] == 0:
+                if self.board[i][j][0] == 0:
                     self.empty_cells.append((i,j))
 
     def apply_move(self, move):
@@ -65,16 +72,16 @@ class Board:
 
     def _shift_left(self):
         for i in range(4):
-            tmp = [non_zero for non_zero in self.board[i] if non_zero!=0] \
-                            + [zero for zero in self.board[i] if zero==0]
+            tmp = [non_zero for non_zero in self.board[i] if non_zero[0] != 0] \
+                            + [zero for zero in self.board[i] if zero[0] == 0]
             if tmp != self.board[i]:
                 self.board[i] = tmp
                 self.changed = True
 
     def _shift_right(self):
         for i in range(4):
-            tmp = [zero for zero in self.board[i] if zero == 0] \
-                    + [non_zero for non_zero in self.board[i] if non_zero != 0]
+            tmp = [zero for zero in self.board[i] if zero[0] == 0] \
+                    + [non_zero for non_zero in self.board[i] if non_zero[0] != 0]
             if tmp != self.board[i]:
                 self.board[i] = tmp
                 self.changed = True
@@ -82,51 +89,55 @@ class Board:
     def _merge_left(self, op):
         for i in range(4):
             for j in range(3):
-                if self.board[i][j] > 0 and self.board[i][j] == self.board[i][j+1]:
+                if self.board[i][j][0] > 0 and self.board[i][j][0] == self.board[i][j+1][0]:
                     self.changed = True
                     if op == 'ADD':
-                        self.board[i][j] += self.board[i][j+1]
+                        self.board[i][j][0] += self.board[i][j+1][0]
                     elif op == 'SUBTRACT':
-                        self.board[i][j] -= self.board[i][j+1]
+                        self.board[i][j][0] -= self.board[i][j+1][0]
                     elif op == 'MULTIPLY':
-                        self.board[i][j] *= self.board[i][j+1]
+                        self.board[i][j][0] *= self.board[i][j+1][0]
                     else:
-                        self.board[i][j] /= self.board[i][j+1]
-                    self.board[i][j+1] = 0
+                        self.board[i][j][0] /= self.board[i][j+1][0]
+                    self.board[i][j][1].update(self.board[i][j+1][1])
+                    self.board[i][j+1] = [0, set()]
 
     def _merge_right(self, op):
         for i in range(4):
             for j in range(3,0,-1):
-                if self.board[i][j] > 0 and self.board[i][j] == self.board[i][j-1]:
+                if self.board[i][j][0] > 0 and self.board[i][j][0] == self.board[i][j-1][0]:
                     self.changed = True
                     if op == 'ADD':
-                        self.board[i][j] += self.board[i][j-1]
+                        self.board[i][j][0] += self.board[i][j-1][0]
                     elif op == 'SUBTRACT':
-                        self.board[i][j] -= self.board[i][j-1]
+                        self.board[i][j][0] -= self.board[i][j-1][0]
                     elif op == 'MULTIPLY':
-                        self.board[i][j] *= self.board[i][j-1]
+                        self.board[i][j][0] *= self.board[i][j-1][0]
                     else:
-                        self.board[i][j] /= self.board[i][j-1]
-                    self.board[i][j-1] = 0
+                        self.board[i][j][0] /= self.board[i][j-1][0]
+                    self.board[i][j][0].update(self.board[i][j-1][0])
+                    self.board[i][j-1] = [0, set()]
 
     def apply_assign(self, assign):
         val = assign[1]
         i, j = assign[2]
-        self.board[i-1][j-1] = val
+        self.board[i-1][j-1][0] = val
     
     def apply_name(self, name):
         i, j = name[2]
-        self.name_map[(i-1,j-1)] = name[1]
+        if name[1] not in self.all_names:
+            self.all_names.add(name[1])
+            self.board[i-1][j-1][1].add(name[1])
 
     def apply_query(self, query):
         i, j = query[1]
-        return self.board[i-1][j-1]
+        return self.board[i-1][j-1][0]
 
     def print_board(self):
         for i in range(4):
             print('----'*8)
             for j in range(4):
-                val = self.board[i][j]
+                val = self.board[i][j][0]
                 if val == 0:
                     val = ' '
                     space = '     '
@@ -162,8 +173,10 @@ board.apply_assign(('assign', 46, (3, 4)))
 print()
 board.print_board()
 board.apply_name(('name', 'xyz', (4, 3)))
+board.apply_name(('name', 'abcd', (4, 3)))
 print()
-print(board.name_map)
+print(board.board)
+print(board.all_names)
 res = board.apply_query(('query', (4, 1)))
 print()
 print(res)
