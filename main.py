f7ba7e17a684f2122ec66b0a431fa40894cec0f3
chetan22 <ck.chetan20@gmail.com>
2021-04-16 00:59:26 +0530

    error handling completed. Refactored code, added comments

--- main.py
+++ main.py
@@ -24,25 +24,8 @@ reserved = {
     'IN': 'IN',
 }
 
-# list of token names
+# list of other tokens
 tokens = [
-    # 'ADD',
-    # 'SUBTRACT',
-    # 'MULTIPLY',
-    # 'DIVIDE',
-    # 'LEFT',
-    # 'RIGHT',
-    # 'UP',
-    # 'DOWN',
-    # 'ASSIGN',
-    # 'TO',
-    # 'VAR',
-    # 'IS',
-    # 'QUERY',
-    # 'POS',
-    # 'VARNAME',
-    # 'VAL',
-    # 'FULLSTOP',
     'ID',
     'NUMBER',
     'COMMA',
@@ -52,34 +35,12 @@ tokens = [
 
 tokens += list(reserved.values())
 
-# token
-# t_ADD       = r'ADD'
-# t_SUBTRACT  = r'SUBTRACT'
-# t_MULTIPLY  = r'MULTIPLY'
-# t_DIVIDE    = r'DIVIDE'
-# t_LEFT      = r'LEFT'
-# t_RIGHT     = r'RIGHT'
-# t_UP        = r'UP'
-# t_DOWN      = r'DOWN'
-# t_ASSIGN    = r'ASSIGN'
-# t_TO        = r'TO'
-# t_VAR       = r'VAR'
-# t_IS        = r'IS'
-# t_QUERY     = r'VALUE IN'
-# t_POS       = r'[1234],[1234]'
-
-# def t_POS(t):
-#     r'[1234],[1234]'
-#     return t
+# regex for lexer
 
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z0-9]*'
     t.type = reserved.get(t.value, 'ID')
     return t
-
-# def t_VAL(t):
-#     r'[0-9]+'
-#     return t
 
 def t_NUMBER(t):
     r'[0-9]+'
@@ -94,6 +55,7 @@ def t_FULLSTOP(t):
     r'\.'
     return t
 
+# currently unused, but added as per spec sheet
 def t_QUESTIONMARK(t):
     r'\?'
     return t
@@ -103,18 +65,23 @@ t_ignore = ' \t'
 def t_error(t):
     print('Illegal token in lexer') # only for debugging, remove later
     t.lexer.skip(1)
+    print_on_stderr()
 
 # build the lexer
 lexer = lex.lex()
 
+# cfg for parser
+
+# top level rule
 def p_game(p):
     '''
     game : statement
-         | warn
+         | nofullstop
+         | invalidvarname FULLSTOP
     '''
-    if str(p.slice[1]) == 'warn':
-        print('You should end a command with a fullstop.')
-    else:
+    # if object starts with a 'warn' then there was some error detection in lower levels
+    # error msg has been displayed therefore we do not process this
+    if not p[1][0].startswith('warn'):
         run(p[1])
 
 def p_statement(p):
@@ -123,15 +90,6 @@ def p_statement(p):
               | assign FULLSTOP
               | name FULLSTOP
               | query FULLSTOP
-    '''
-    p[0] = p[1]
-
-def p_warn(p):
-    '''
-    warn : move
-         | assign
-         | name
-         | query
     '''
     p[0] = p[1]
 
@@ -160,52 +118,114 @@ def p_assign(p):
     '''
     assign : ASSIGN NUMBER TO NUMBER COMMA NUMBER
     '''
-    p[0] = ('assign', p[2], (p[4], p[6]))
+    # check if <<x>>,<<y>> are valid indices
+    # otherwise prefix 'warn' to p[0] which is passed to the next level up
+    if not (1 <= p[4] <= 4 and 1 <= p[6] <= 4):
+        print('2048> There is not tile like that. The tile co-ordinates must be in the range 1,2,3,4.')
+        p[0] = ('warn_assign', p[2], (p[4], p[6]))
+        print_on_stderr()
+    else:
+        p[0] = ('assign', p[2], (p[4], p[6]))
 
 def p_name(p):
     '''
     name : VAR ID IS NUMBER COMMA NUMBER
     '''
-    p[0] = ('name', p[2], (p[4], p[6]))
+    # check if <<x>>,<<y>> are valid indices
+    # otherwise prefix 'warn' to p[0] which is passed to the next level up
+    if not (1 <= p[4] <= 4 and 1 <= p[6] <= 4):
+        print('2048> There is not tile like that. The tile co-ordinates must be in the range 1,2,3,4.')
+        p[0] = ('warn_name', p[2], (p[4], p[6]))
+        print_on_stderr()
+    else:
+        p[0] = ('name', p[2], (p[4], p[6]))
 
 def p_query(p):
     '''
     query : VALUE IN NUMBER COMMA NUMBER
     '''
-    p[0] = ('query', (p[3], p[5]))
+    # check if <<x>>,<<y>> are valid indices
+    # otherwise prefix 'warn' to p[0] which is passed to the next level up
+    if not (1 <= p[3] <= 4 and 1 <= p[5] <= 4):
+        print('2048> There is not tile like that. The tile co-ordinates must be in the range 1,2,3,4.')
+        p[0] = ('warn_query', (p[3], p[5]))
+        print_on_stderr()
+    else:
+        p[0] = ('query', (p[3], p[5]))
+
+def p_invalidvarname(p):
+    '''
+    invalidvarname : VAR error IS NUMBER COMMA NUMBER
+    '''
+    # we handle this separately via error recovery process (parser resynchronization)
+    print('2048> Syntax error!')
+    print('2048> Invalid token for variable name')
+    p[0] = ('warn_namevar', p[2], (p[4], p[6]))
+
+    # no print_on_stderr() here since it's already done in p_error()
+
+def p_nofullstop(p):
+    '''
+    nofullstop : move
+               | assign
+               | name
+               | query
+               | invalidvarname
+    '''
+    # we cannot handle errors at the end of the input sentence
+    # therefore we added it as a separate rule and explicitly marked an syntax error.
+    print('2048> Syntax error!')
+    print('2048> You should end a command with a fullstop.')
+    p[0] = ('warn_fullstop', p[1])
+    print_on_stderr()
 
 def p_error(p):
-    print('Syntac error!')
+    # each time a parser is unable to match the current token with any rule
+    # this method is called.
+    # error recovery begins from this method.
+    # parser will try to search in rules that match upto before the
+    # erraneous token and 'error' token after that. (see p_invalidvarname rule)
+    print('2048> Sorry, I do not understand that.')
+    print_on_stderr()
 
+# build the parser
 parser = yacc.yacc()
 
-def run(ast):
+def run(stmt):
     board = global_env['board']
-    if ast[0] == 'move':
-        resp = board.apply_move(ast)
+    if stmt[0] == 'move':
+        resp = board.apply_move(stmt)
         if resp['status'] == 1:
-            print('2048> Thanks,', ast[2].lower(), 'move done, random tile added.')
+            print('2048> Thanks,', stmt[2].lower(), 'move done, random tile added.')
             print('2048> The current state is:')
             board.print_board()
             print_on_stderr(board.board)
-    elif ast[0] == 'assign':
-        resp = board.apply_assign(ast)
+        else:
+            print_on_stderr()
+    elif stmt[0] == 'assign':
+        resp = board.apply_assign(stmt)
         if resp['status'] == 1:
             print('2048> Thanks, assignment done.')
             print('2048> The current state is:')
             board.print_board()
             print_on_stderr(board.board)
-    elif ast[0] == 'name':
-        resp = board.apply_name(ast)
+        else:
+            print_on_stderr()
+    elif stmt[0] == 'name':
+        resp = board.apply_name(stmt)
         if resp['status'] == 1:
             print('2048> Thanks, naming done.')
             print_on_stderr(board.board)
+        else:
+            print_on_stderr()
     else:
-        resp = board.apply_query(ast)
+        resp = board.apply_query(stmt)
         if resp[status] == 1:
             print('2048> Thanks, querying done')
             print('2048> Current value:', resp['data']['value'])
             print_on_stderr(board.board)
+        else:
+            print_on_stderr()
 
 def print_on_stderr(board=None):
     if board:
@@ -225,16 +245,14 @@ def print_on_stderr(board=None):
 if __name__ == '__main__':
     board = Board()
     global_env['board'] = board
-    board.initiate_new_board()        
+    board.initiate_new_board()
     print('2048> Hi, I am 2048-game Engine.')
     print('2048> The start state is:')
     board.print_board()
+    print_on_stderr(board.board)
     while 1:
         try:
             inp = input('2048> Please type a command.\n----> ')
-            # lexer.input(inp)
-            # for tok in lexer:
-            #     print(tok)
             parser.parse(inp)
         except KeyboardInterrupt:
             print()
